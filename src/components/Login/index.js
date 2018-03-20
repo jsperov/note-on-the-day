f6a16b96f0731198fd5ff6d8a14a5cb7c76ac793
jsperov <jsperov@gmail.com>
2018-03-20 18:53:00 +0300

    update

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import { Button, Checkbox, Input } from 'semantic-ui-react'
+import { Form } from '../Form'
 import 'semantic-ui-css/semantic.min.css'
 
 export default class Login extends React.Component {
-    render() {
-        return (
-            <div id="form-box">
-                <h1>Вход</h1>
-                <div className="field-block">
-                    <form id="field-block__form">
-                        <div className="field__element-wrap">
-                            <Input placeholder='Введите имя' />
-                        </div>
-                        <div className="field__element-wrap">
-                            <Input type="password" placeholder='Введите пароль' />
-                        </div>
-                        <div className="field__element-wrap">
-                            <Checkbox label={<label>Запомнить меня</label>} />
-                        </div>
-                        <div className="field__element-wrap">
-                            <Button type="submit">
-                                Войти
-                            </Button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="form-box">
+        <h1>Вход</h1>
+        <Form className="field-block">
+          <div className="field__element-wrap">
+            <Form.Field validate={['required', 'isNumber']} name="login" placeholder="Введите имя" component={Input} />
+          </div>
+          <div className="field__element-wrap">
+            <Form.Field
+              validate={['isPasswordStrength']}
+              name="password"
+              placeholder="Введите пароль"
+              component={Input}
+            />
+          </div>
+          <div className="field__element-wrap">
+            <Form.Field name="remember" label="Запомнить меня" component={Checkbox} />
+          </div>
+          <div className="field__element-wrap">
+            <Button type="submit">Войти</Button>
+          </div>
+        </Form>
+      </div>
+    )
+  }
 }
