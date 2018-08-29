bb650e200173dc1030017bf73e7f2566abf93e71
aperov <perov@reksoft.ru>
2018-08-29 15:28:48 +0300

    changed eslint file and refactoring

--- src/components/Form/rules.js
+++ src/components/Form/rules.js
@@ -1,41 +1,42 @@
-import REG_EXP from './const'
+import { isEmpty } from 'lodash';
+import REG_EXP from './const';
 
 const rules = {
   required: value => ({
     error: 'field should not be empty',
     value,
-    isValid: value ? true : false
+    isValid: !isEmpty(value),
   }),
 
   email: value => ({
     error: 'not correct email',
     value,
-    isValid: REG_EXP.EMAIL.test(value) ? true : false
+    isValid: REG_EXP.EMAIL.test(value),
   }),
 
   isNumber: value => ({
     error: 'NaN',
     value,
-    isValid: REG_EXP.NUMBER.test(value) ? true : false
+    isValid: REG_EXP.NUMBER.test(value),
   }),
 
   isPhoneNumber: value => ({
     error: 'incorrect number',
     value,
-    isValid: REG_EXP.PHONE_NUMBER.test(value) ? true : false
+    isValid: REG_EXP.PHONE_NUMBER.test(value),
   }),
 
   isPasswordStrength: value => ({
     error: 'the password is not reliable',
     value,
-    isValid: REG_EXP.CHECK_PASSWORD.test(value) ? true : false
+    isValid: REG_EXP.CHECK_PASSWORD.test(value),
   }),
 
   isPIN: value => ({
     error: 'incorrect PIN code',
     value,
-    isValid: REG_EXP.PIN_CODE.test(value) ? true : false
+    isValid: REG_EXP.PIN_CODE.test(value),
   })
 }
 
-export default rules
+export default rules;
